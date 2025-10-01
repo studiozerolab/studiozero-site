4be432d884def69ee0194452253192f6ab5d5fe6
topher6835 <sears.topher@gmail.com>
2025-10-01 16:27:57 -0400

    added 404, removed comments

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -3,5 +3,5 @@ export type NavLink = { label: string; href: string };
 export const navLinks: NavLink[] = [
   { label: "Services", href: "/services" },
   { label: "Packages",     href: "/packages" },
-  { label: "Contact",  href: "#contact" },
+  { label: "Contact",  href: "/#contact" },
 ];
